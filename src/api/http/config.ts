b2498e02b10ecb39062789ff2ed3565c7043bbca
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2026-05-28 09:20:17 +0800

    fix/apiIsDev (#2)

--- src/api/http/config.ts
+++ src/api/http/config.ts
@@ -1,6 +1,7 @@
 import config from "@/config"
 
-const isDev = import.meta.env.MODE === 'dev'
+const isDev = import.meta.env.MODE === 'development'
+console.log(import.meta.env.MODE);
 
 const urlApi = isDev ? config.serverUrl : '/'
 
